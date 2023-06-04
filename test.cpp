12eccd21ddde6de9f1f1308b5675366fc71bd8f3
hjc2000 <hjc.666@foxmail.com>
2023-06-05 00:10:10 +0800

    1

--- test.cpp
+++ test.cpp
@@ -13,37 +13,24 @@ using std::fstream;
 using std::ios_base;
 using std::string;
 
-extern "C"
+void test_dic()
 {
-	void test_dic()
-	{
-		FFmpeg::AVDictionary dic;
-		dic.av_dict_set("name", "hjc");
-		cout << dic.av_dict_count() << endl;
-		auto entry = dic.av_dict_get("name", nullptr);
-		cout << entry->key << endl;
-		cout << entry->value << endl;
-	}
-
-	void test_parse()
-	{
-		int height, width;
-		av_parse_video_size(&height, &width, "1920*1080");
-		cout << height << ":" << width << endl;
-	}
+	FFmpeg::AVDictionary dic;
+	dic.av_dict_set("name", "hjc");
+	cout << dic.av_dict_count() << endl;
+	auto entry = dic.av_dict_get("name", nullptr);
+	cout << entry->key << endl;
+	cout << entry->value << endl;
 }
 
-void hello(void);
+void test_parse()
+{
+	int height, width;
+	av_parse_video_size(&height, &width, "1920*1080");
+	cout << height << ":" << width << endl;
+}
 
 int main(void)
 {
-	// fstream fs{"./temp.txt", ios_base::out | ios_base::in | ios_base::trunc};
-	// fs << "黄锦灿" << endl;
-	// fs.flush();
-	// fs.seekg(0);
-	// string read_str;
-	// fs >> read_str;
-	// cout << read_str << endl;
-	hello();
 	return 0;
 }
